3aeac3653aad66a99a10f863ecd406911c0d9b51
Yi Yang <yyang@narval1.narval.calcul.quebec>
2025-01-28 13:38:57 -0500

    update order settings

--- order_forest_timing.sh
+++ order_forest_timing.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Slurm parameters
-MEMO=120G       # memory allocation
-TIME=0-10:00:00 # time allocation
+MEMO=40G       # memory allocation
+TIME=0-06:00:00 # time allocation
 CORE=1          # core allocation
 
 CHDIR=/home/yyang/scratch/grf-experiments
