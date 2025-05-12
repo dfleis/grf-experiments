8704ca1b5ab269ba340177c576f1b31b8e747586
dfleis <david.p.fleischer@gmail.com>
2025-05-12 17:20:47 -0400

    fix typo

--- run-mse-forest.R
+++ run-mse-forest.R
@@ -25,7 +25,7 @@ settings <- validate_setting(model_type = model_type, setting_id = setting_id)
 
 #----- Data params
 Kvals <- c(4, 16, 64, 256)
-pvals <- c5
+pvals <- 5
 nvals <- as.integer(c(1e4, 2e4, 5e4, 1e5, 2e5))
 n_test <- 5000
 
