c6b0c9ab2460e4e9f1eccc9ae7434213fc1999bb
David Fleischer <dfleis2@math-jcn2.CS.McGill.CA>
2025-05-17 00:14:52 -0400

    copy files to run smaller scale forest benchmarks on a different server

---
+++ run-bench-forest-FORK.R
@@ -0,0 +1,56 @@
+source("utils/utils-bench-forest.R")
+seed <- 1
+FILENAME_HEAD <- "bench-forest-FORK"
+NUM_THREADS <- 1
+
+args <- commandArgs(TRUE)
+model_type <- args[1] # "vcm" or "hte"
+setting_id <- args[2] # "1", "2", "3", "4", "5"
+
+# model_type <- "vcm"
+# setting_id <- c(1, 2, 3, 4)
+stumps <- c(TRUE, FALSE)
+Kvals <- c(4, 16, 64, 256)
+pvals <- 5
+nvals <- as.integer(c(1e4, 2e4, 1e5, 2e5))
+numtreevals <- c(100, 250)
+
+stopifnot(model_type %in% c("vcm", "hte"))
+stopifnot(setting_id %in% c("1", "2", "3", "4", "5"))
+
+methods <- c("grad", "fpt1", "fpt2")
+center_data <- TRUE
+nrep <- 1
+niter <- 3
+.quiet <- FALSE
+
+grf_args_global <- list(
+  min.node.size = 5,
+  sample.fraction = 0.5,
+  honesty = TRUE,
+  honesty.fraction = 0.5,
+  honesty.prune.leaves = TRUE,
+  alpha = 0.05,
+  ci.group.size = 1,
+  compute.oob.predictions = FALSE,
+  num.threads = NUM_THREADS,
+  seed = seed
+)
+
+bench_forest(
+  methods = methods,
+  model_type = model_type,
+  setting_id = setting_id,
+  numtreevals = numtreevals,
+  Kvals = Kvals,
+  pvals = pvals,
+  nvals = nvals,
+  center_data = center_data,
+  grf_args = grf_args_global,
+  nrep = nrep,
+  niter = niter,
+  seed = seed,
+  filename_head = FILENAME_HEAD,
+  .quiet = .quiet
+)
+
---
+++ run_bench_forest_hte_FORK.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+MODEL=hte
+TIMESTAMP=$(date +%Y%m%d_%H%M%S)
+
+# Create logs directory if it doesn't exist
+mkdir -p logs/
+
+# Define function to run script and log output
+run_and_log() {
+    local script=$1
+    local setting=$2
+    local script_base=$(basename "$script" .R | sed 's/run-//')
+    
+    local logfile="logs/${script_base}_${MODEL}_setting${setting}_${TIMESTAMP}.log"
+    echo "Running $script_base model=$MODEL setting=$setting (logging to $logfile)"
+    Rscript "$script" $MODEL $setting > "$logfile" 2>&1
+}
+
+# Run all benchmarks with settings 1-5
+for setting in {1..5}; do
+    run_and_log "run-bench-forest-small-FORK.R" $setting
+done
+
+#for setting in {1..5}; do
+#    run_and_log "run-bench-forest-FORK.R" $setting
+#done
